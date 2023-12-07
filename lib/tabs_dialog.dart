5ea9dfa4e96e7dcde181213c9f7ae752980f5b17
Magno_Daza <magnodaza1@gmail.com>
2023-12-07 02:43:22 +0000

    toggle en lugar de segmentbutton

--- lib/tabs_dialog.dart
+++ lib/tabs_dialog.dart
@@ -1,26 +1,36 @@
-//file tabdialog.dart
+// Importaciones necesarias
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter/cupertino.dart';
 import 'icon_list.dart';
 import 'tab_provider.dart';
 import 'tab_preview.dart';
+import 'package:provider/provider.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
+// Clase principal
 class TabDialog extends StatefulWidget {
+  // Variables para almacenar el índice del tab y si es un nuevo tab
   final int? tabIndex;
   final bool isNewTab;
 
+  // Constructor
   const TabDialog({this.tabIndex, this.isNewTab = false});
 
+  // Creación del estado
   @override
   _TabDialogState createState() => _TabDialogState();
 }
 
+// Clase de estado
 class _TabDialogState extends State<TabDialog> {
+  // Variables para almacenar el controlador del texto, el ícono y el valor del dropdown
   TextEditingController? _textController;
   IconData? _icon;
-  String dropdownValue = 'Mostrar icono y nombre';
+  int segmentedControlGroupValue = 0;
+  // Define un GlobalKey para el formulario
+  final _formKey = GlobalKey<FormState>();
 
+  // Inicialización del estado
   @override
   void initState() {
     super.initState();
@@ -36,12 +46,14 @@ class _TabDialogState extends State<TabDialog> {
     }
   }
 
+  // Función para manejar la selección del ícono
   void _handleIconSelected(IconData selectedIcon) {
     setState(() {
       _icon = selectedIcon;
     });
   }
 
+  // Construcción de la interfaz de usuario
   @override
   Widget build(BuildContext context) {
     var tabProvider = Provider.of<TabProvider>(context);
@@ -69,15 +81,14 @@ class _TabDialogState extends State<TabDialog> {
             TabPreview(
               textController: _textController!,
               icon: _icon!,
-              showText: dropdownValue != 'Solo el icono',
-              showIcon: dropdownValue != 'Solo el texto',
+              showText: segmentedControlGroupValue != 2,
+              showIcon: segmentedControlGroupValue != 1,
               textSize: textSize,
               indicatorSize: indicatorSize,
             ),
             const SizedBox(height: 15),
             Divider(),
             const SizedBox(height: 15),
-            // Resto del código...
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -106,124 +117,89 @@ class _TabDialogState extends State<TabDialog> {
             ),
             Divider(),
             const SizedBox(height: 15),
-            TextField(
-              controller: _textController,
-              decoration: const InputDecoration(hintText: "Nombre del Tab"),
-            ),
-            const SizedBox(height: 15),
-            Divider(),
-            const SizedBox(height: 15),
-            const Text(
-              'Mostrar en el tab',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 15),
-            DropdownButton<String>(
-              value: dropdownValue,
-              onChanged: (String? newValue) {
-                setState(() {
-                  dropdownValue = newValue!;
-                });
-              },
-              items: <String>[
-                'Mostrar icono y nombre',
-                'Solo el texto',
-                'Solo el icono',
-              ].map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
-            const SizedBox(height: 15),
-            Divider(),
-            const SizedBox(height: 15),
-            // Eliminamos el código relacionado con el tamaño del tab
+            // Aquí es donde se reemplaza el TextField por un TextFormField con un OutlineInputBorder
             // Resto del código...
+            Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    controller: _textController,
+                    decoration: InputDecoration(
+                      labelText: "Nombre del Tab",
+                      border: OutlineInputBorder(),
+                      // Agregamos un icono al principio del TextFormField
+                      prefixIcon: Icon(Icons.edit),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Por favor, proporciona un nombre para el tab';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 15),
+                  Divider(),
+                  const SizedBox(height: 15),
+                  // Aquí es donde se reemplaza el CupertinoSegmentedControl por un ToggleButtons
+                  const Text(
+                    'Mostrar en el tab',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 15),
+                  ToggleButtons(
+                    children: <Widget>[
+                      Text('Icono y texto' +
+                          (segmentedControlGroupValue == 0 ? ' ✔️' : '')),
+                      Text('Icono' +
+                          (segmentedControlGroupValue == 1 ? ' ✔️' : '')),
+                      Text('Texto' +
+                          (segmentedControlGroupValue == 2 ? ' ✔️' : '')),
+                    ],
+                    isSelected: [
+                      segmentedControlGroupValue == 0,
+                      segmentedControlGroupValue == 1,
+                      segmentedControlGroupValue == 2,
+                    ],
+                    onPressed: (int index) {
+                      setState(() {
+                        for (int i = 0; i < 3; i++) {
+                          if (i == index) {
+                            segmentedControlGroupValue = i;
+                          }
+                        }
+                      });
+                    },
+                  ),
+                  const SizedBox(height: 15),
+                  Divider(),
+                  const SizedBox(height: 15),
+                  TextButton(
+                    child: Text(widget.isNewTab
+                        ? 'Crear nuevo tab'
+                        : 'Confirmar edición'),
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        // Resto del código...
+                        // Notificación de la creación del tab
+                        Fluttertoast.showToast(
+                          msg: "Tab creado",
+                          toastLength: Toast.LENGTH_SHORT,
+                          gravity: ToastGravity.BOTTOM,
+                        );
+                      }
+                    },
+                  ),
+                  TextButton(
+                    child: const Text('Cancelar'),
+                    onPressed: () => Navigator.of(context).pop(),
+                  ),
+                ],
+              ),
+            ),
+// Resto del código...
           ],
         ),
-        actions: <Widget>[
-          TextButton(
-            child: const Text('Cancelar'),
-            onPressed: () => Navigator.of(context).pop(),
-          ),
-          TextButton(
-            child:
-                Text(widget.isNewTab ? 'Crear nuevo tab' : 'Confirmar edición'),
-            onPressed: () {
-              if (_textController != null &&
-                  _icon != null &&
-                  _textController!.text.isNotEmpty) {
-                int? tabIndex;
-                if (!widget.isNewTab) {
-                  tabIndex = widget.tabIndex!;
-                  Provider.of<TabProvider>(context, listen: false).updateTab(
-                    tabIndex,
-                    _textController!.text,
-                    _icon!,
-                  );
-                  Fluttertoast.showToast(
-                    msg: "Tab actualizado",
-                    toastLength: Toast.LENGTH_SHORT,
-                    gravity: ToastGravity.BOTTOM,
-                  );
-                } else {
-                  bool tabExists =
-                      Provider.of<TabProvider>(context, listen: false)
-                          .myTabs
-                          .any((tab) => tab.text == _textController!.text);
-                  if (tabExists) {
-                    Fluttertoast.showToast(
-                      msg: "No se pueden tener tabs con el mismo nombre",
-                      toastLength: Toast.LENGTH_SHORT,
-                      gravity: ToastGravity.BOTTOM,
-                    );
-                  } else {
-                    Provider.of<TabProvider>(context, listen: false).addTab(
-                      _textController!.text,
-                      _icon!,
-                    );
-                    tabIndex = Provider.of<TabProvider>(context, listen: false)
-                            .myTabs
-                            .length -
-                        1;
-                    Fluttertoast.showToast(
-                      msg: "Tab creado",
-                      toastLength: Toast.LENGTH_SHORT,
-                      gravity: ToastGravity.BOTTOM,
-                    );
-                    _textController!.clear();
-                    FocusScope.of(context).unfocus();
-                  }
-                }
-                if (dropdownValue == 'Solo el texto') {
-                  Provider.of<TabProvider>(context, listen: false)
-                      .updateTabShowIcon(tabIndex!, false);
-                  Provider.of<TabProvider>(context, listen: false)
-                      .updateTabShowText(tabIndex!, true);
-                } else if (dropdownValue == 'Solo el icono') {
-                  Provider.of<TabProvider>(context, listen: false)
-                      .updateTabShowIcon(tabIndex!, true);
-                  Provider.of<TabProvider>(context, listen: false)
-                      .updateTabShowText(tabIndex!, false);
-                } else {
-                  Provider.of<TabProvider>(context, listen: false)
-                      .updateTabShowIcon(tabIndex!, true);
-                  Provider.of<TabProvider>(context, listen: false)
-                      .updateTabShowText(tabIndex!, true);
-                }
-              } else {
-                Fluttertoast.showToast(
-                  msg: "Por favor, proporciona un nombre para el tab",
-                  toastLength: Toast.LENGTH_SHORT,
-                  gravity: ToastGravity.BOTTOM,
-                );
-              }
-              Navigator.of(context).pop();
-            },
-          ),
-        ],
       ),
     );
   }
